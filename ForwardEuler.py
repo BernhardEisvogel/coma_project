f5ecbd7b269ca35fdfe1cac8679b413ddd8eeb33
BernhardEisvogel <BernhardEisvogel@users.noreply.github.com>
2020-06-08 15:55:37 +0200

    Update
    Aufgabe 1

--- ForwardEuler.py
+++ ForwardEuler.py
@@ -20,7 +20,7 @@ import math
 
 def ForwardEuler(fun, y0, T,n):
     Schritt = y0           #Initialisieren
-    for i in range(n+1): 
+    for i in range(n): 
         Schritt = Schritt +  T/n*fun(T/n * i, Schritt)
     return Schritt
 
@@ -28,7 +28,7 @@ def ForwardKutta2_mod(fun, y0, T, n):
     # Diese Methode ist auch bekannt als modifizierte Euler'sche Methode
     Schritt = y0           #Initialisieren
     h = T/n
-    for i in range(n+1): 
+    for i in range(n): 
         Schritt = Schritt +  h* fun(h* i + 0.5 * h , Schritt + 
                                      0.5 * h * fun (h* i, Schritt))
     return Schritt
@@ -37,24 +37,31 @@ def ForwardKutta2_imp(fun, y0, T, n):
     # Diese Methode ist auch bekannt als verbesserte Euler'sche Methode
     Schritt = y0           #Initialisieren
     h = T/n
-    for i in range(n+1): 
+    for i in range(n): 
                 Schritt = Schritt +  h * (0.5*fun(h*i,Schritt) + 
                                         0.5* fun (i*h + 0.5*h, Schritt 
                                                 + h))
     return Schritt
-    
-def ForwardKutta3(fun, y0, T, n):
-    # Runge Kutta Methode dritter Ordnung
+def ForwardKutta4(fun, y0, T, n):
+    '''ForwardKutta4 nimmt als Parameter: \n
+        fun, Ableitung der Funktion abhänjgig von x und y \n
+        y0,  Anfangswert \n
+        T, Endzeitpunkt \n
+        n, Anzahl der Iterationsschritte \n
+        
+        Als Approximationsverfahren wird ein Vierstufiges Runge Kutta Verfahren
+        angewendet
+        '''    
+    n = n+1
     Schritt = y0           #Initialisieren
     h = T/n
-    for i in range(n+1): 
+    for i in range(n): 
         g1 = fun(h * i, Schritt)
-        g2 =  fun(h*(i+0.5),Schritt + h/2 * g1)
-        g3 =  fun(h*(i+0.5),Schritt + h/2 * g2)
-        g4 =  fun(h*i,Schritt + h * g3)
+        g2 =  fun(h*(i+1/2),Schritt + h * 1/2 * g1)
+        g3 =  fun(h*(i+1/2),Schritt + h * 1/2 * g2)
+        g4 =  fun(h * i + h,Schritt + h *       g3)
         Schritt = Schritt +  T/n*(1/6 * g1+ 1/3 * g2 + 1/3 * g3 + 1/6 *g4)
     return Schritt
-
 
 
 
@@ -63,7 +70,7 @@ def TestF(x,y): return float(pow(math.e,x))
 print("modifizierte Euker:", ForwardKutta2_mod(TestF, 0,6,1000))
 print("verbesserte Euker:", ForwardKutta2_imp(TestF, 0,6,1000))
 print("Euler:", ForwardEuler(TestF, 0,6,1000))
-print("Kutta3:", ForwardKutta3(TestF, 0,6,1000))
+print("Kutta4:", ForwardKutta4(TestF, 0,6,1000))
 
 print(pow(math.e,6)-1)
 
--- methods.py
+++ methods.py
@@ -8,13 +8,37 @@ Created on Mon Jun  8 15:33:03 2020
 
 
 def ForwardEuler(fun, y0, T,n):
-    '''Forwar Euler nimmt als Parameter: \n
+    '''Forward Euler nimmt als Parameter: \n
         fun, Ableitung der Funktion abhänjgig von x und y \n
         y0,  Anfangswert \n
         T, Endzeitpunkt \n
-        n, Anzahl der Iterationsschritte'''
-        
+        n, Anzahl der Iterationsschritte
+         \n
+        Als Approximationsverfahren wird das Euelerverfahren benutzt
+        '''
+    n = n+1 # Es sollen n + 1 Punkte diskretisiert werden  
     Schritt = y0           
-    for i in range(n+1): 
+    for i in range(n): 
         Schritt = Schritt +  T/n*fun(T/n * i, Schritt)
+    return Schritt
+
+def ForwardKutta4(fun, y0, T, n):
+    '''ForwardKutta4 nimmt als Parameter: \n
+        fun, Ableitung der Funktion abhänjgig von x und y \n
+        y0,  Anfangswert \n
+        T, Endzeitpunkt \n
+        n, Anzahl der Iterationsschritte \n
+        
+        Als Approximationsverfahren wird ein Vierstufiges Runge Kutta Verfahren
+        angewendet
+        '''    
+    n = n+1                 # Es sollen n + 1 Punkte diskretisiert werden
+    Schritt = y0           #Initialisieren
+    h = T/n
+    for i in range(n): 
+        g1 = fun(h * i, Schritt)
+        g2 =  fun(h*(i+1/2),Schritt + h * 1/2 * g1)
+        g3 =  fun(h*(i+1/2),Schritt + h * 1/2 * g2)
+        g4 =  fun(h * i + h,Schritt + h *       g3)
+        Schritt = Schritt +  T/n*(1/6 * g1+ 1/3 * g2 + 1/3 * g3 + 1/6 *g4)
     return Schritt
