47d06c501b0f9a65ff764fe64eae32722f41410f
BernhardEisvogel <BernhardEisvogel@users.noreply.github.com>
2020-06-08 15:40:05 +0200

    Update methods.py

--- methods.py
+++ methods.py
@@ -8,6 +8,12 @@ Created on Mon Jun  8 15:33:03 2020
 
 
 def ForwardEuler(fun, y0, T,n):
+    '''Forwar Euler nimmt als Parameter: \n
+        fun, Ableitung der Funktion abhänjgig von x und y \n
+        y0,  Anfangswert \n
+        T, Endzeitpunkt \n
+        n, Anzahl der Iterationsschritte'''
+        
     Schritt = y0           
     for i in range(n+1): 
         Schritt = Schritt +  T/n*fun(T/n * i, Schritt)
