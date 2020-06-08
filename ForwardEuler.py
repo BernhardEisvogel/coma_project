bfe57c9642607b5f687457518f86ad95de489cc1
BernhardEisvogel <64311469+BernhardEisvogel@users.noreply.github.com>
2020-06-08 14:01:59 +0200

    Update EulerForward.py
    Dieses Programm enthält die Eulerapproximierung

--- ForwardEuler.py
+++ ForwardEuler.py
@@ -5,10 +5,7 @@ Created on Mon Jun  8 13:56:28 2020
 
 @author: be
 """
-
-# Dieses Programm enthält die Eulerapproximierung und eine Runge-Kutta
-# Approximierung 2. und 3.Grades
-#ForwardEuler(fun, y0, T,n)
+# Dieses Programm enthält die Eulerapproximierung
 
 def ForwardEuler(fun, y0, T,n):
     return 0
