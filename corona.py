47aec3af766148e378fb698b13f2bfcc8b60829b
BernhardEisvogel <BernhardEisvogel@users.noreply.github.com>
2020-06-10 22:44:05 +0200

    Update

--- corona.py
+++ corona.py
@@ -10,37 +10,42 @@ import numpy as np ## nump schon in Folgen importiert
 import matplotlib.pyplot as plot
 from matplotlib.lines import Line2D 
 
-def Berechnung(x,y):
-    return x,y-0.1
 
-#def Phasenportrait():
-xstartwerte= []
-ystartwerte = []
-for s in np.arange(0.0,1.05,0.1):
-    xstartwerte.append(1-s) # Anfällige
-    ystartwerte.append(s)   # Infektiös 
-plot.plot(xstartwerte,ystartwerte ,color = 'b')
-for i in range(len(xstartwerte)):
-    x= np.array([])
-    y= np.array([])
-    xw=xstartwerte[i] 
-    yw=ystartwerte[i]
-    while yw>0.000001:
-        x = np.append(x,[xw])
-        y = np.append(y,[yw])
-        xw,yw = methods.epidlös(3,xw,yw)
-    
-    plot.plot(x,y,'b--')
-    if(len(x) != 0 and len(y) != 0):
-        line = plot.plot(x,y,'b--')[0]
-        methods.AddArrow(line, position = 0.05, color='b')
-    
-plot.ylim(0, 1)
-plot.xlim(0, 1)
-plot.title("Phasenportrait des SIR Modells")
-plot.xlabel("Anteil Anfällige s")
-plot.ylabel("Anteil Infektiöse i")
-plot.show
+def Phasenportrait():
+    gamma,beta0,T,s0,i0 = methods.SirLesen()
+    xstartwerte= []
+    ystartwerte = []
+    for s in np.arange(0.0,1.05,0.1):
+        xstartwerte.append(1-s) # Anfällige
+        ystartwerte.append(s)   # Infektiös 
+    plot.plot(xstartwerte,ystartwerte ,color = 'b')
+    for i in range(len(xstartwerte)):
+        x= np.array([])
+        y= np.array([])
+        xw=xstartwerte[i] 
+        yw=ystartwerte[i]
+        while yw>0.000001:
+            x = np.append(x,[xw])
+            y = np.append(y,[yw])
+            xw,yw, a = methods.epidlös(gamma, beta0, 3,xw,yw,0)
+            # besitzt keine weitere Bedeutung verhindert aber, 
+            # dass eine zweite FUnktion geschrieben werden muss
+        
+        plot.plot(x,y,'b--')
+        if(len(x) != 0 and len(y) != 0):
+            line = plot.plot(x,y,'b--')[0]
+            methods.AddArrow(line, position = 0.05, color='b')
+        
+    plot.ylim(0, 1)
+    plot.xlim(0, 1)
+    plot.title("Phasenportrait des SIR Modells")
+    plot.xlabel("Anteil Anfällige s")
+    plot.ylabel("Anteil Infektiöse i")
+    plot.show
 
-#Phasenportrait()
+gamma, beta0, my, T, s0,i0 = methods.SirDynLesen()
+#my = 0.00003
+print("Infizierte in D nach", T, " Tagen: ", methods.endlös(my,gamma, beta0, T,s0,i0,0) )
+print("Infizierte in D nach", T, " Tagen: ", methods.epidlös(gamma, beta0, T,s0,i0,0) )
+
 
--- methods.py
+++ methods.py
@@ -6,7 +6,6 @@ Created on Mon Jun  8 15:33:03 2020
 @author: Louisa Weber, Bernhard Eisvogel
 """
 import numpy as np
-import math
 
 def SirLesen():
     '''
@@ -37,66 +36,6 @@ def SirLesen():
     file.close()
     return gamma,beta0,T,s0,i0
 
-gamma,beta0,T,s0,i0 = SirLesen()
-def ableitung(t,y):
-    '''
-    berechnet einen Vektor der Änderunsraten von s und i
-
-    Parameters
-    ----------
-    t : float, Zeitpunkt an dem die Ableitung berechnet wird
-    y : array, y[1] Anzahl Anfälliger s(t)\n
-    y[2] Anzahl Infizierter i(t)
-
-    Returns
-    -------
-    Array, enthält Änderungsrate Anfälliger s und \n
-    Änderungsrate Infizierter i
-
-    '''
-    dS=(-1)*beta0*y[0]*y[1] #Ableitungsfunktion von s(t)
-    dI=beta0*y[0]*y[1]-gamma*y[1] #Ableitungsfunktiono von i(t)
-    return np.array([dS,dI])
-
-def ForwardEuler(fun, y0, T,n):
-    '''Forward Euler nimmt als Parameter: \n
-        fun, Ableitung der Funktion abhänjgig von x und y \n
-        y0,  Anfangswert \n
-        T, Endzeitpunkt \n
-        n, Anzahl der Iterationsschritte
-         \n
-        Als Approximationsverfahren wird das Euelerverfahren benutzt
-        '''
-    n = n+1 # Es sollen n + 1 Punkte diskretisiert werden  
-    Schritt = y0           
-    for i in range(n): 
-        Schritt = Schritt +  T/n*fun(T/n * i, Schritt)
-    return Schritt
-
-def ForwardKutta4(fun, y0, T, n):
-    '''ForwardKutta4 nimmt als Parameter: \n
-        fun, Ableitung der Funktion abhänjgig von x und y \n
-        y0,  Anfangswert \n
-        T, Endzeitpunkt \n
-        n, Anzahl der Iterationsschritte \n
-        
-        Als Approximationsverfahren wird ein Vierstufiges Runge Kutta Verfahren
-        angewendet
-        '''    
-    n = n+1                 # Es sollen n + 1 Punkte diskretisiert werden
-    Schritt = y0           #Initialisieren
-    h = T/n
-    for i in range(n): 
-        g1 = fun(h * i, Schritt)
-        g2 =  fun(h*(i+1/2),Schritt + h * 1/2 * g1)
-        g3 =  fun(h*(i+1/2),Schritt + h * 1/2 * g2)
-        g4 =  fun(h * i + h,Schritt + h *       g3)
-        Schritt = Schritt +  T/n*(1/6 * g1+ 1/3 * g2 + 1/3 * g3 + 1/6 *g4)
-    return Schritt
-
-
-
-
 def SirDynLesen():
     '''
     Dieser Funktion gibt das aus der Datei "sirdyn.param" gelesen Tupel zurück
@@ -121,15 +60,57 @@ def SirDynLesen():
         #einzelne Parameter werden getrennt und in Liste gespeichert
         data+=i.split() 
     #konvertieren der Parameter zu floats
-    for j in (0,len(data)-1):
-        data[j]=float(data[j])
-        
-    #Bezeichnung der Parameter
-    gamma, beta0, my, T, s0,i0 =data[0],data[1],data[2],data[3],data[4],data[5]
+    gamma=float(data[0])
+    beta0=float(data[1])
+    my=float(data[2])
+    T=float(data[3])
+    s0=float(data[4])
+    i0=float(data[5])
     file.close()
     return gamma, beta0, my, T, s0,i0
 
-def epidlös(t,s_0,i_0):
+
+def ForwardEuler(fun, y0, T,n):
+    '''Forward Euler nimmt als Parameter: \n
+        fun, Ableitung der Funktion abhängig von x und y \n
+        y0,  Anfangswert \n
+        T, Endzeitpunkt \n
+        n, Anzahl der Iterationsschritte
+         \n
+        Als Approximationsverfahren wird das Euelerverfahren benutzt
+        '''
+    n = n+1 # Es sollen n + 1 Punkte diskretisiert werden  
+    Schritt = y0           
+    for i in range(n): 
+        Schritt = Schritt +  T/n*fun(T/n * i, Schritt)
+    return Schritt
+
+def ForwardKutta4(fun, y0, T, n):
+    '''ForwardKutta4 nimmt als Parameter: \n
+        fun, Ableitung der Funktion abhänjgig von x und y \n
+        y0,  Anfangswert \n
+        T, Endzeitpunkt \n
+        n, Anzahl der Iterationsschritte \n
+        
+        Als Approximationsverfahren wird ein Vierstufiges Runge Kutta Verfahren
+        angewendet
+        '''    
+    n = n+1                 # Es sollen n + 1 Punkte diskretisiert werden
+    Schritt = y0            #Initialisieren
+    h = T/n
+    
+    for i in range(n): 
+        g1 = fun(h * i, Schritt)
+        g2 =  fun(h*(i+1/2),Schritt + h * 1/2 * g1)
+        g3 =  fun(h*(i+1/2),Schritt + h * 1/2 * g2)
+        g4 =  fun(h * i + h,Schritt + h *       g3)
+        Schritt = Schritt +  T/n*(1/6 * g1+ 1/3 * g2 + 1/3 * g3 + 1/6 *g4)
+    return Schritt
+
+
+
+
+def epidlös(gamma, beta0, t,s_0,i_0,r_0):
     '''
     Lösung des epidemischen Modells unter Benutzung von Euler \n
     Funktion der Anfälligen s(t) \n
@@ -137,6 +118,9 @@ def epidlös(t,s_0,i_0):
 
     Parameters
     ----------
+    gamma : float, Inverse der Durchschnittlichen Zeit, \n
+    in der ein erkrankter ansteckend ist
+    beta0 : float, Kontaktrate
     t : float, Zeitpunkt  
     s0 : float, Anfangswerte Anfällige 
     i0 : float, Anfangswert Infizierte
@@ -147,11 +131,51 @@ def epidlös(t,s_0,i_0):
     und Infizierte i(t)
 
     '''
-    y_0=np.array([s_0,i_0])
+    y_0=np.array([s_0,i_0,r_0])
     if t==0:
         return y_0
     else:
-        return ForwardEuler(ableitung,y_0,t,2000)
+        return ForwardEuler(lambda t,y: 
+                            np.array([(-1)*beta0*y[0]*y[1],
+                                      beta0*y[0]*y[1]-gamma*y[1], gamma * y[1]]),
+                                     y_0,
+                                     t,
+                                     2000)
+    #n gross wählen!!!
+            
+def endlös(my, gamma, beta0, t,s_0,i_0, r_0):
+    '''
+    Lösung des epidemischen Modells unter Benutzung von Euler \n
+    Funktion der Anfälligen s(t) \n
+    und Infizierten i(t) 
+
+    Parameters
+    ----------
+    N: Anzahl der Menschen
+    gamma : float, Inverse der Durchschnittlichen Zeit, \n
+    in der ein erkrankter ansteckend ist
+    beta0 : float, Kontaktrate
+    t : float, Zeitpunkt  
+    s0 : float, Anfangswerte Anfällige 
+    i0 : float, Anfangswert Infizierte
+
+    Returns
+    -------
+    array, enthält Anzahl Anfällige s(t) \n
+    und Infizierte i(t)
+
+    '''
+    y_0 = np.array([s_0,i_0,r_0])
+    if t==0:
+        return y_0
+    else:
+        return ForwardEuler(lambda t,y: 
+                            np.array([my - my* y[0]-beta0*y[0]*y[1],
+                                      (-1) * my * i_0 + beta0*y[0]*y[1]-gamma * y[1],
+                                      (-1) * my*y[2]+ gamma * y[1]]),
+                                     y_0,
+                                     t,
+                                     2000)
     #n gross wählen!!!
     
 """
@@ -159,6 +183,7 @@ Created on Thu Jun  4 06:30:03 2020
 
 @author: Lena
 """
+
 
 def AddArrow(line, position=None, direction='right', size=15, color=None):
     '''
