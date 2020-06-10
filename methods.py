ef9200a6d2fc6e3febffe49484d423f52339438b
BernhardEisvogel <BernhardEisvogel@users.noreply.github.com>
2020-06-10 15:29:20 +0200

    Update
    Update

--- methods.py
+++ methods.py
@@ -42,3 +42,65 @@ def ForwardKutta4(fun, y0, T, n):
         g4 =  fun(h * i + h,Schritt + h *       g3)
         Schritt = Schritt +  T/n*(1/6 * g1+ 1/3 * g2 + 1/3 * g3 + 1/6 *g4)
     return Schritt
+
+def SirLesen():
+    '''
+    Dieser Funktion gibt das aus der Datei "sir.param" gelesen Tupel zurück
+    -------
+    gamma : FLOAT
+        Erster Wert in [0]
+    beta0 : FLOAT
+        Zweiter Wert in [1]
+    T : FLOAT
+        Dritter Wert in [2]
+    s0 : FLOAT
+        Vierter Wert in [3]
+    i0 : FLOAT
+        Fünfter Wert in [4]
+        '''
+        
+    file=open("sir.param","r")
+    data=[]
+    for i in file:
+        #einzelne Parameter werden getrennt und in Liste gespeichert
+        data+=i.split() 
+    #konvertieren der Parameter zu floats
+    for j in (0,len(data)-1):
+        data[j]=float(data[j])
+        
+    #Bezeichnung der Parameter
+    gamma,beta0,T,s0,i0=data[0],data[1],data[2],data[3],data[4]
+    file.close()
+    return gamma,beta0,T,s0,i0
+
+def SirDynLesen():
+    '''
+    Dieser Funktion gibt das aus der Datei "sirdyn.param" gelesen Tupel zurück
+    -------
+    gamma : FLOAT
+        Erster Wert in [0]
+    beta0 : FLOAT
+        Zweiter Wert in [1]
+    my : FLOAT
+        Dritter Wert in [2]
+    T : FLOAT
+        Vierter Wert in [3]
+    s0 : FLOAT
+        Fünfter Wert in [4]
+    i0 : FLOAT
+        Sechster Wert in [5]
+
+    '''
+    file=open("sirdyn.param","r")
+    data=[]
+    for i in file:
+        #einzelne Parameter werden getrennt und in Liste gespeichert
+        data+=i.split() 
+    #konvertieren der Parameter zu floats
+    for j in (0,len(data)-1):
+        data[j]=float(data[j])
+        
+    #Bezeichnung der Parameter
+    gamma, beta0,my,T, s0,i0 =data[0],data[1],data[2],data[3],data[4],data[5]
+    file.close()
+    return gamma, beta0, my, T, s0,i0
